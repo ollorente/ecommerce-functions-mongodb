9bea3e5ab20213837c78d506768417f0242071f5
Óscar Llorente <llorente_oscar@hotmail.com>
2021-06-15 16:54:53 -0500

    Agregando header y footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,23 @@
 <template>
   <div>
+    <TheNavbar />
     <Nuxt />
+    <TheFooter />
   </div>
 </template>
 
-<style></style>
+<script>
+import TheFooter from '~/components/AtomicDesign/Organisms/TheFooter'
+import TheNavbar from '~/components/AtomicDesign/Organisms/TheNavbar'
+
+export default {
+  components: {
+    TheFooter,
+    TheNavbar,
+  },
+}
+</script>
+
+<style>
+@import url('https://use.fontawesome.com/releases/v5.7.2/css/all.css');
+</style>

a3ffc2f1910912509cb2cce35cf695b1aa2cf3a2
Óscar Llorente <llorente_oscar@hotmail.com>
2021-06-15 16:53:33 -0500

    Linting

--- functions/app.js
+++ functions/app.js
@@ -1,45 +1,46 @@
-require("dotenv").config()
-const express = require("express")
-const path = require("path")
-const cookieParser = require("cookie-parser")
-const logger = require("morgan")
-const multer = require("multer")
-const serverless = require("serverless-http")
-const mongoose = require("mongoose")
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const multer = require('multer')
+const serverless = require('serverless-http')
+const mongoose = require('mongoose')
 
 const app = express()
 
-mongoose.connect(
-    process.env.MONGODB, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+mongoose
+  .connect(process.env.MONGODB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log(`>>> [DB] is connected... <<<`))
   .catch((error) => console.log(`<<< [ERROR]: ${error} >>>`))
 
-app.use(logger("dev"))
+app.use(logger('dev'))
 app.use(express.json())
-app.use(express.urlencoded({
-  extended: false
-}))
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+)
 app.use(cookieParser())
 app.use(
   multer({
     storage: multer.diskStorage({}),
     fileFilter: (req, file, cb) => {
       if (!file.mimetype.match(/jpg|jpeg|png|gif$i/)) {
-        cb(new Error("<<< File not supported!. >>>"), false)
+        cb(new Error('<<< File not supported!. >>>'), false)
       }
       cb(null, true)
     },
-    dest: path.join(__dirname, "/public/temp"),
+    dest: path.join(__dirname, '/public/temp'),
     limits: {
       fileSize: 1024 * 1024 * 2,
     },
-  }).single("image")
+  }).single('image')
 )
 
 app.use('/.netlify/functions/app', require('./routes'))

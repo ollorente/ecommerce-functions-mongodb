5314f9ac7468fa50026392326a1d4f2594083503
Óscar Llorente <llorente_oscar@hotmail.com>
2021-06-15 16:55:58 -0500

    Diseñando

--- pages/cuenta/login.vue
+++ pages/cuenta/login.vue
@@ -1,75 +1,86 @@
 <template>
-  <div class="row">
-    <div class="col-sm-6">
-      <div class="well">
-        <h2>Nuevo cliente</h2>
-        <p><strong>Registrar Cuenta</strong></p>
-        <p>
-          Al crear una cuenta, podrá comprar más rápido, estar actualizado sobre el estado de un pedido y realizar un seguimiento de los pedidos que haya realizado anteriormente.
-        </p>
-        <a
-          href="/cuenta/registrarse"
-          class="btn btn-primary"
-          >Continuar</a
-        >
-      </div>
-    </div>
-    <div class="col-sm-6">
-      <div class="well">
-        <h2>Soy Cliente</h2>
-        <p><strong>Yo soy cliente</strong></p>
-        <form @submit.prevent="login" >
-          <div class="form-group">
-            <label class="control-label" for="input-email"
-              >Dirección de correo electrónico</label
-            >
-            <input
-              type="text"
-              name="email"
-              value=""
-              placeholder="Dirección de correo electrónico"
-              id="input-email"
-              class="form-control"
-            />
+  <div>
+    <ImageFeature :image="image" />
+    <div class="container">
+      <main class="grid col-12 col-sm-2">
+        <div class="col-sm-6">
+          <div class="well">
+            <TitleH2 :title2="title1" />
+            <p><strong>Registrar Cuenta</strong></p>
+            <p>
+              Al crear una cuenta, podrá comprar más rápido, estar actualizado
+              sobre el estado de un pedido y realizar un seguimiento de los
+              pedidos que haya realizado anteriormente.
+            </p>
+            <a href="/cuenta/registrarse" class="btn btn-primary">Continuar</a>
           </div>
-          <div class="form-group">
-            <label class="control-label" for="input-password">Contraseña</label>
-            <input
-              type="password"
-              name="password"
-              value=""
-              placeholder="Contraseña"
-              id="input-password"
-              class="form-control"
-            />
-            <a
-              href="/cuenta/olvide-mi-contrasena"
-              >Contraseña olvidada</a
-            >
+        </div>
+        <div class="col-sm-6">
+          <div class="well">
+            <TitleH2 :title2="title2" />
+            <p><strong>Yo soy cliente</strong></p>
+            <form @submit.prevent="login">
+              <div class="form-group">
+                <label class="control-label" for="input-email"
+                  >Dirección de correo electrónico</label
+                >
+                <input
+                  type="text"
+                  name="email"
+                  value=""
+                  placeholder="Dirección de correo electrónico"
+                  id="input-email"
+                  class="form-control"
+                />
+              </div>
+              <div class="form-group">
+                <label class="control-label" for="input-password"
+                  >Contraseña</label
+                >
+                <input
+                  type="password"
+                  name="password"
+                  value=""
+                  placeholder="Contraseña"
+                  id="input-password"
+                  class="form-control"
+                />
+                <a href="/cuenta/olvide-mi-contrasena">Contraseña olvidada</a>
+              </div>
+              <input type="submit" value="Login" class="btn btn-primary" />
+              <input type="hidden" name="redirect" value="/cuenta/account" />
+            </form>
           </div>
-          <input type="submit" value="Login" class="btn btn-primary" />
-          <input
-            type="hidden"
-            name="redirect"
-            value="/cuenta/account"
-          />
-        </form>
-      </div>
+        </div>
+      </main>
     </div>
   </div>
 </template>
 
 <script>
+import TitleH2 from '~/components/AtomicDesign/Atoms/TitleH2'
+
 export default {
-    data() {
-        return {
-            form: {}
-        }
-    },
-    methods: {
-        async login() {
-            console.log('Login...')
-        }
+  components: {
+    TitleH2
+  },
+  data() {
+    return {
+      form: {},
+      title1: {
+        name: 'Nuevo cliente',
+        class: 'title h3',
+      },
+      title2: {
+        name: 'Soy Cliente',
+        class: 'title h3',
+      },
     }
+  },
+  methods: {
+    async login() {
+      console.log('Login...')
+    },
+  },
 }
 </script>
--- pages/cuenta/olvide-mi-contrasena.vue
+++ pages/cuenta/olvide-mi-contrasena.vue
@@ -1,50 +1,62 @@
 <template>
-  <div id="content" class="col-sm-9">
-    <h1>Forgot Your Password?</h1>
-    <p>
-      Enter the e-mail address associated with your account. Click submit to
-      have a password reset link e-mailed to you.
-    </p>
-    <form @submit.prevent="forgotten" class="form-horizontal">
-      <fieldset>
-        <legend>Your E-Mail Address</legend>
-        <div class="form-group required">
-          <label class="col-sm-2 control-label" for="input-email"
-            >E-Mail Address</label
-          >
-          <div class="col-sm-10">
-            <input
-              type="text"
-              name="email"
-              value=""
-              placeholder="E-Mail Address"
-              id="input-email"
-              class="form-control"
-            />
-          </div>
+  <div>
+    <!-- <FormLogup /> -->
+    <div class="container">
+      <main class="grid col-3">
+        <div class="span-2">
+          <TitleH1 :title1="title1" />
+          <p>
+            Enter the e-mail address associated with your account. Click submit
+            to have a password reset link e-mailed to you.
+          </p>
+          <form @submit.prevent="forgotten" class="form-horizontal">
+            <fieldset>
+              <legend>Your E-Mail Address</legend>
+              <div class="form-group required">
+                <label class="col-sm-2 control-label" for="input-email"
+                  >E-Mail Address</label
+                >
+                <div class="col-sm-10">
+                  <input
+                    type="text"
+                    name="email"
+                    value=""
+                    placeholder="E-Mail Address"
+                    id="input-email"
+                    class="form-control"
+                  />
+                </div>
+              </div>
+            </fieldset>
+            <div class="buttons clearfix">
+              <div class="pull-left">
+                <a href="/cuenta/login" class="btn btn-default">Back</a>
+              </div>
+              <div class="pull-right">
+                <input type="submit" value="Continue" class="btn btn-primary" />
+              </div>
+            </div>
+          </form>
         </div>
-      </fieldset>
-      <div class="buttons clearfix">
-        <div class="pull-left">
-          <a
-            href="/cuenta/login"
-            class="btn btn-default"
-            >Back</a
-          >
-        </div>
-        <div class="pull-right">
-          <input type="submit" value="Continue" class="btn btn-primary" />
-        </div>
-      </div>
-    </form>
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
+import TitleH1 from '~/components/AtomicDesign/Atoms/TitleH1'
+
 export default {
+  components: {
+    TitleH1,
+  },
   data() {
     return {
       form: {},
+      title1: {
+        name: 'Forgot Your Password?',
+        class: 'title h3',
+      },
     }
   },
   methods: {

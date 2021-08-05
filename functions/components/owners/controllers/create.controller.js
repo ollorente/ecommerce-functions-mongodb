730ba379124bd22b7cec7540def046082a36d716
Óscar Llorente <llorente_oscar@hotmail.com>
2021-08-05 12:00:16 -0500

    Linting

--- functions/components/owners/controllers/create.controller.js
+++ functions/components/owners/controllers/create.controller.js
@@ -4,12 +4,11 @@ const OwnerModel = require('../model')
 module.exports = async (req, res, next) => {
   let result
   try {
-    result = await OwnerModel.find({})
+    result = await OwnerModel.find()
 
     res.status(200).json({
       error: false,
-      data:
-				result.length > 0 ? result.map((e) => OwnerDTO.RefInterface(e)) : []
+      data: result.length > 0 ? result.map((e) => OwnerDTO.RefInterface(e)) : []
     })
   } catch (err) {
     next(err)

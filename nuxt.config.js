5da73f57218adab78b7063eb2b512661572f6a79
Óscar Llorente <llorente_oscar@hotmail.com>
2021-08-16 16:27:47 -0500

    CDN de Bootstrap

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,6 +70,14 @@ export default {
         href: 'https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap',
       },
     ],
+    script: [
+      {
+        src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
+        integrity:
+          'sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM',
+        crossorigin: 'anonymous',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css

6783fd535be50ee1bb4b80d1dfdf0fb29cd6fecd
Óscar Llorente <llorente_oscar@hotmail.com>
2021-08-16 17:10:33 -0500

    Se agrega molécula

---
+++ components/AtomicDesign/Molecules/ListGroupFlush.vue
@@ -0,0 +1,18 @@
+<template>
+	<ul class="list-group list-group-flush">
+		<li class="list-group-item bg-transparent px-0" v-for="(menu, index) in menus" :key="index">
+			<nuxt-link :to="menu.link" class="nav__link text-light">
+				<i :class="menu.icon"></i> {{ menu.title }}
+			</nuxt-link>
+		</li>
+	</ul>
+</template>
+
+<script>
+export default {
+	name: 'ListGroupFlush',
+	props: ['menus'],
+}
+</script>
+
+<style scoped></style>

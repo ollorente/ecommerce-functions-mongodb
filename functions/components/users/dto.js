e93f5d102adf423cbed118c5005ae4067c1ae55d
Óscar Llorente <llorente_oscar@hotmail.com>
2021-08-16 13:56:56 -0500

    Ajustando variables

--- functions/components/users/dto.js
+++ functions/components/users/dto.js
@@ -1,9 +1,4 @@
 module.exports.Interface = (db) => {
-  const { PersonDTO } = require('../person')
-  const { MeetingDTO } = require('../meeting')
-  const { ProjectDTO } = require('../project')
-  const { TaskDTO } = require('../task')
-
   return {
     id: String(db._id),
     name: db.name ? String(db.name) : '',
@@ -12,14 +7,6 @@ module.exports.Interface = (db) => {
     phone: db.phone ? String(db.phone) : '',
     photoURL: db.photoURL ? String(db.photoURL) : '',
     gravatar: db.gravatar ? String(db.gravatar) : '',
-    projects: db._projects.map((e) => ProjectDTO.RefInterface(e)),
-    projectsCount: Number(db._projectsCount),
-    meetings: db._meetings.map((e) => MeetingDTO.RefInterface(e)),
-    meetingsCount: Number(db._meetingsCount),
-    tasks: db._tasks.map((e) => TaskDTO.RefInterface(e)),
-    tasksCount: Number(db._tasksCount),
-    people: db._people.map((e) => PersonDTO.RefInterface(e)),
-    peopleCount: Number(db._peopleCount),
     isActive: Boolean(db.isActive),
     isLock: Boolean(db.isLock),
     createdAt: db.createdAt,

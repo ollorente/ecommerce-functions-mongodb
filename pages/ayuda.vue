79abcb1b216940933b1e96024b73a610f37ff390
Óscar Llorente <llorente_oscar@hotmail.com>
2021-08-16 14:06:20 -0500

    SEO

--- pages/ayuda.vue
+++ pages/ayuda.vue
@@ -12,6 +12,19 @@
 import TitleH1 from '~/components/AtomicDesign/Atoms/TitleH1'
 
 export default {
+	head: {
+		htmlAttrs: {
+			lang: 'es',
+		},
+		title: 'Ayuda © CiudadBusca.co',
+		meta: [
+			{
+				hid: 'description',
+				name: 'description',
+				content: 'Ayuda',
+			},
+		],
+	},
 	layout: 'main',
 	components: {
 		TitleH1,

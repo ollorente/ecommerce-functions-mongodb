7e12c34c8a7fa83d0a98fcf51b0dc48272e934e6
Óscar Llorente <llorente_oscar@hotmail.com>
2021-08-16 23:31:31 -0500

    Restructurando menú

--- components/AtomicDesign/Molecules/HeaderMenu.vue
+++ components/AtomicDesign/Molecules/HeaderMenu.vue
@@ -1,85 +1,116 @@
 <template>
-	<div class="header">
-		<div class="container">
-			<div class="nav__bottom">
-				<div class="nav__bottom--left">
-					<a href="#" class="nav__bottom__link">
-						<i class="fas fa-map-marker-alt"></i>
-						<div class="bottom-body">
-							<span>Ingresa tu</span>
-							<span>ubicación</span>
-						</div>
-					</a>
-					<ul class="nav__navbar">
-						<li class="nav__item">
-							<span class="nav__link" @click="getCategories">Categorías</span>
-						</li>
-						<li class="nav__item" v-for="menu in main_menus" :key="menu.id">
-							<nuxt-link
-								:to="`/${router}/${menu.link}`"
-								class="nav__link"
-								>{{ menu.title }}</nuxt-link
-							>
-						</li>
-					</ul>
-				</div>
-				<div class="nav__bottom--right">
-					<ul class="nav__navbar">
-						<li class="nav__item" v-for="menu in option_menus" :key="menu.id">
-							<nuxt-link :to="`${menu.link}`" class="nav__link">{{
-								menu.title
-							}}</nuxt-link>
-						</li>
-					</ul>
+	<div>
+		<nav
+			class="navbar sticky-top navbar-expand-sm navbar-light"
+			style="background: #fded35"
+		>
+			<div class="container">
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<NavbarNav :links="linksLeft" class="me-auto mb-2 mb-lg-0" />
+					<NavbarNav :links="linksRight" class="d-flex" />
 				</div>
 			</div>
-		</div>
+		</nav>
 	</div>
 </template>
 
 <script>
+import NavbarNav from '~/components/AtomicDesign/Molecules/NavbarNav'
+
 export default {
 	name: 'HeaderMenu',
+	components: {
+		NavbarNav,
+	},
 	data() {
 		return {
 			router: 'bima',
-			main_menus: [
+			linksLeft: [
 				{
-					id: '1',
+					dropdown: false,
+					icon: 'fas fa-map-marker-alt',
+					link: '/',
+					subLinks: [],
+					title: 'Ubicación',
+				},
+				{
+					dropdown: true,
+					icon: '',
+					link: `/${this.$route.params.store}/categorias`,
+					subLinks: [
+						{
+							icon: '',
+							link: `/${this.$route.params.store}/categorias/bebes`,
+							title: 'Bebes',
+						},
+						{
+							icon: '',
+							link: `/${this.$route.params.store}/categorias/bebes`,
+							title: 'Bebes',
+						},
+					],
+					title: 'Categorías',
+				},
+				{
+					dropdown: false,
+					icon: '',
+					link: `/${this.$route.params.store}/ofertas`,
+					subLinks: [],
 					title: 'Ofertas',
-					link: '/ofertas',
 				},
 				{
-					id: '2',
+					dropdown: false,
+					icon: '',
+					link: `/${this.$route.params.store}/historial`,
+					subLinks: [],
 					title: 'Historial',
-					link: '/historial',
 				},
 				{
-					id: '3',
+					dropdown: false,
+					icon: '',
+					link: `/${this.$route.params.store}/moda`,
+					subLinks: [],
 					title: 'Moda',
-					link: '/moda',
 				},
 				{
-					id: '4',
-					title: 'Vender',
-					link: '/vender',
-				},
-				{
-					id: '5',
-					title: 'Ayuda /PQR',
-					link: '/ayuda',
+					dropdown: false,
+					icon: '',
+					link: `/${this.$route.params.store}/ayuda`,
+					subLinks: [],
+					title: 'Ayuda/PQR',
 				},
 			],
-			option_menus: [
+			linksRight: [
 				{
-					id: '1',
-					title: 'Opción 1',
-					link: '/opcion-1',
+					dropdown: false,
+					icon: '',
+					link: `/opcion`,
+					subLinks: [],
+					title: 'Opción',
 				},
 				{
-					id: '2',
-					title: 'Opción 2',
-					link: '/opcion-2',
+					dropdown: true,
+					icon: '',
+					link: `#`,
+					subLinks: [
+						{
+							icon: '',
+							link: `/perfil`,
+							title: 'Perfil',
+						},
+					],
+					title: 'Usuario',
 				},
 			],
 		}
@@ -92,115 +123,4 @@ export default {
 }
 </script>
 
-<style scoped>
-.header {
-	width: 100%;
-	height: 56px;
-	margin-top: 56px;
-	padding: calc(var(--unit) / 2) 0;
-	background: var(--primary);
-}
-
-.nav__bottom {
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-}
-
-.nav__bottom--left {
-	display: flex;
-	align-items: center;
-}
-
-.nav__bottom__link {
-	flex: 1;
-	width: 100%;
-	margin: 0;
-	padding: calc(var(--unit) / 3) var(--unit);
-	font-size: 1rem;
-	color: var(--dark);
-	border: solid 1px transparent;
-	border-radius: calc(var(--unit) / 4);
-	cursor: pointer;
-	display: flex;
-	align-items: center;
-}
-
-.nav__bottom__link:hover {
-	border: solid 1px var(--dark);
-}
-
-.nav__bottom__link i {
-	font-size: calc(var(--unit) * 1.5);
-	margin-right: calc(var(--unit) / 2);
-}
-
-.bottom-body {
-}
-
-.bottom-body span:first-child {
-	display: block;
-	font-size: 0.8rem;
-	font-weight: 400;
-}
-
-.bottom-body span:last-child {
-	display: block;
-	font-size: 0.9rem;
-	font-weight: 700;
-}
-
-.nav__bottom--right {
-}
-
-.nav__navbar {
-	display: none;
-	margin: 0;
-	padding: 0;
-}
-
-.nav__item {
-	margin: 0;
-	padding: 0;
-	list-style: none;
-	display: inline-block;
-}
-
-.nav__link {
-	width: 100%;
-	margin: 0;
-	padding: calc(var(--unit) / 2);
-	font-size: 1rem;
-	color: var(--dark);
-	border: solid 1px transparent;
-	border-radius: calc(var(--unit) / 4);
-	cursor: pointer;
-}
-
-.nav__link:hover {
-	border: solid 1px var(--dark);
-}
-
-/* Small devices (landscape phones, 576px and up) */
-@media (min-width: 576px) {
-}
-
-/* Medium devices (tablets, 768px and up) */
-@media (min-width: 768px) {
-}
-
-/* Large devices (desktops, 992px and up) */
-@media (min-width: 992px) {
-	.nav__navbar {
-		display: block;
-	}
-}
-
-/* X-Large devices (large desktops, 1200px and up) */
-@media (min-width: 1200px) {
-}
-
-/* XX-Large devices (larger desktops, 1400px and up) */
-@media (min-width: 1400px) {
-}
-</style>
+<style scoped></style>

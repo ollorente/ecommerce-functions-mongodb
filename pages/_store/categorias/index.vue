42664d3ed7ba510db7795d4ad617d039297507e1
Óscar Llorente <llorente_oscar@hotmail.com>
2021-08-17 15:06:42 -0500

    Ajustando diseño básico

--- pages/_store/categorias/index.vue
+++ pages/_store/categorias/index.vue
@@ -1,9 +1,52 @@
 <template>
-	<div class="container">
+	<div class="container mt-3">
 		<div class="row">
 			<div class="col-12">
-				<h1>Categorías</h1>
+				<TitleH1 :title1="title1" />
 			</div>
 		</div>
 	</div>
 </template>
+
+<script>
+import TitleH1 from '~/components/AtomicDesign/Atoms/TitleH1'
+
+export default {
+	head: {
+		htmlAttrs: {
+			lang: 'es',
+		},
+		title: 'Categorías © CiudadBusca.co',
+		meta: [
+			{
+				hid: 'description',
+				name: 'description',
+				content: 'Categorías',
+			},
+		],
+	},
+	layout: 'store',
+	components: {
+		TitleH1,
+	},
+	data() {
+		return {
+			title1: {
+				name: 'Categorías',
+				class: 'title h3',
+			},
+		}
+	},
+	created() {
+		this.getCategories()
+	},
+	methods: {
+		async getCategories() {},
+	},
+	watch: {
+		$route: ['getCategories'],
+	},
+}
+</script>
+
+<style scoped></style>
--- pages/_store/productos/index.vue
+++ pages/_store/productos/index.vue
@@ -1,3 +1,52 @@
 <template>
-	<h1>Productos</h1>
+	<div class="container mt-3">
+		<div class="row">
+			<div class="col-12">
+				<TitleH1 :title1="title1" />
+			</div>
+		</div>
+	</div>
 </template>
+
+<script>
+import TitleH1 from '~/components/AtomicDesign/Atoms/TitleH1'
+
+export default {
+	head: {
+		htmlAttrs: {
+			lang: 'es',
+		},
+		title: 'Productos © CiudadBusca.co',
+		meta: [
+			{
+				hid: 'description',
+				name: 'description',
+				content: 'Productos',
+			},
+		],
+	},
+	layout: 'store',
+	components: {
+		TitleH1,
+	},
+	data() {
+		return {
+			title1: {
+				name: 'Productos',
+				class: 'title h3',
+			},
+		}
+	},
+	created() {
+		this.getProducts()
+	},
+	methods: {
+		async getProducts() {},
+	},
+	watch: {
+		$route: ['getProducts'],
+	},
+}
+</script>
+
+<style scoped></style>

97b55cd87b4a8c8ca1e9140ed9ec8bab59b273c0
Óscar Llorente <llorente_oscar@hotmail.com>
2021-08-16 23:32:06 -0500

    Cambiando estilo

--- components/AtomicDesign/Molecules/ListGroupFlush.vue
+++ components/AtomicDesign/Molecules/ListGroupFlush.vue
@@ -1,7 +1,7 @@
 <template>
 	<ul class="list-group list-group-flush">
 		<li class="list-group-item bg-transparent px-0" v-for="(menu, index) in menus" :key="index">
-			<nuxt-link :to="menu.link" class="nav__link text-light">
+			<nuxt-link :to="menu.link" class="text-decoration-none text-light">
 				<i :class="menu.icon"></i> {{ menu.title }}
 			</nuxt-link>
 		</li>
